d047d563b9fbce27fc1b6d4127fa6ce971f77358
unknown <aluno@.(none)>
2010-03-11 20:50:33 -0300

    primeiro commit

---
+++ www/hello_world/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Hello PHP</title>
+	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<style> article, aside, figure, footer, header, hgroup, menu, nav, section { display: block; } </style>
+	
+</head>
+<body>
+	<div class="container">
+		<?php echo '<h1>Meu primeiro PHP!!!</h1>' ?>
+	<p> Copyrigth &copy; <?php echo date ('d/m/Y')  ?></p>
+	<p> Hora <?php echo date ('H:m')  ?></p>
+	</div>
+</body>
+</html>
